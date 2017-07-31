3e38f1dadba7200611985a5e9339ac8d639cd536
dmitry <ulyanovdmitrya@gmail.com>
2017-07-31 12:28:10 +0300

    Small bug fixed

--- lib/fastlane/plugin/gs_android/actions/gs_android_release.rb
+++ lib/fastlane/plugin/gs_android/actions/gs_android_release.rb
@@ -3,7 +3,7 @@ module Fastlane
     class GsAndroidReleaseAction < Action
       def self.run(params)
         version_name = Helper::VersionWorker.getRcVersionName(ENV['versionsFilePostfix']).to_s # TODO: it will be better to parse it from GP
-
+        version_name = version_name[0..-4] #TODO: need some better variant for this shit
         Helper::GooglePlayLoader.update_changelog(ENV['alias'], ENV['app_id'], version_name, 'beta', ENV['locales'], ENV['json_key_file'])
 
         Helper::GsAndroidHelper.run_action(Actions::SupplyAction, track: "beta", track_promote_to: "production", skip_upload_apk: true, skip_upload_metadata: true, skip_upload_images: true, skip_upload_screenshots: true)
