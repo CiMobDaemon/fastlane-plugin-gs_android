dee8f9b29a6e77d62b43f57c0f57a1a8ae7d0797
CiMobDaemon <cimobdaemon@gmail.com>
2018-01-15 17:29:38 +0300

    Update gs_android_beta.rb

--- lib/fastlane/plugin/gs_android/actions/gs_android_beta.rb
+++ lib/fastlane/plugin/gs_android/actions/gs_android_beta.rb
@@ -2,54 +2,55 @@ module Fastlane
   module Actions
     class GsAndroidBetaAction < Action
       def self.run(params)
-				Helper::VersionWorker.increment_version_code(ENV['alias'],
-																										 ENV['build_gradle_file_path'])
-				version_name = Helper::VersionWorker.increment_beta_version_name(ENV['alias'],
-                                                                         ENV['build_gradle_file_path'],
-                                                                         ENV['general_major_version']).to_s
+	Helper::VersionWorker.increment_version_code(ENV['alias'],
+																							 ENV['build_gradle_file_path'])
+	version_name = Helper::VersionWorker.increment_beta_version_name(ENV['alias'],
+						 ENV['build_gradle_file_path'],
+						 ENV['general_major_version']).to_s
 
-				Helper::GsAndroidHelper.generate_release_notes('fileBeta', ENV['alias'], version_name, 'Ru')
-				Helper::GsAndroidHelper.generate_release_notes('fileBeta', ENV['alias'], version_name, 'En')
+	Helper::GsAndroidHelper.generate_release_notes('fileBeta', ENV['alias'], version_name, 'Ru')
+	Helper::GsAndroidHelper.generate_release_notes('fileBeta', ENV['alias'], version_name, 'En')
 
-				text_ru = Helper::FileHelper.read(Helper::GsAndroidHelper::NOTES_PATH_TEMPLATE % {Dir: Dir.pwd, project_alias: ENV['alias'], version_name: version_name, lang: 'Ru'})
-				text_en = Helper::FileHelper.read(Helper::GsAndroidHelper::NOTES_PATH_TEMPLATE % {Dir: Dir.pwd, project_alias: ENV['alias'], version_name: version_name, lang: 'En'})
+	text_ru = Helper::FileHelper.read(Helper::GsAndroidHelper::NOTES_PATH_TEMPLATE % {Dir: Dir.pwd, project_alias: ENV['alias'], version_name: version_name, lang: 'Ru'})
+	text_en = Helper::FileHelper.read(Helper::GsAndroidHelper::NOTES_PATH_TEMPLATE % {Dir: Dir.pwd, project_alias: ENV['alias'], version_name: version_name, lang: 'En'})
 
-				require 'date'
-				current_time = DateTime.now
-				time_string = current_time.strftime '%d.%m.%Y %H : %M'
-				crashlytics_changelog = time_string + "\n" + text_ru + "\n\n" + text_en
-				UI.message('changelog = ' + crashlytics_changelog)
+	require 'date'
+	current_time = DateTime.now
+	time_string = current_time.strftime '%d.%m.%Y %H : %M'
+	crashlytics_changelog = time_string + "\n" + text_ru + "\n\n" + text_en
+	UI.message('changelog = ' + crashlytics_changelog)
 
-				Helper::GsAndroidHelper.run_action(Actions::GradleAction,
-                                           task: 'clean')
+	Helper::GsAndroidHelper.run_action(Actions::GradleAction,
+		   task: 'clean')
 
-				#Some applications have different apk types. We need only universal (if it exists) apk on crashlytics
-				build_type = 'Beta'
+	#Some applications have different apk types. We need only universal (if it exists) apk on crashlytics
+	build_type = 'Beta'
 
-				unless ENV['apkType'].nil?
-							build_type = ENV['apkType'] + build_type
-				end
+	unless ENV['apkType'].nil?
+				build_type = ENV['apkType'] + build_type
+	end
 
-				if ENV['flavor'].nil?
-					Helper::GsAndroidHelper.run_action(Actions::GradleAction,
-                                             task: 'assemble',
-                                             build_type: build_type)
-				else
-					Helper::GsAndroidHelper.run_action(Actions::GradleAction,
-                                             task: 'assemble',
-                                             flavor: ENV['flavor'],
-                                             build_type: build_type)
-				end
+	if ENV['flavor'].nil?
+		Helper::GsAndroidHelper.run_action(Actions::GradleAction,
+		     task: 'assemble',
+		     build_type: build_type)
+	else
+		Helper::GsAndroidHelper.run_action(Actions::GradleAction,
+		     task: 'assemble',
+		     flavor: ENV['flavor'],
+		     build_type: build_type)
+	end
 
-				Helper::GsAndroidHelper.run_action(Actions::CrashlyticsAction,
-                                            notes: crashlytics_changelog,
-                                            groups: ENV['test_group_for_fabric']
-                                          )
+	Helper::GsAndroidHelper.run_action(Actions::CrashlyticsAction,
+		    api_token: ENV['CRASHLYTICS_API_TOKEN'],
+		    notes: crashlytics_changelog,
+		    groups: ENV['test_group_for_fabric']
+		  )
 
-				Helper::VersionWorker.save_version_code(ENV['alias'],
-                                                ENV['build_gradle_file_path'])
-				Helper::VersionWorker.save_beta_version_name(ENV['alias'],
-                                                     ENV['build_gradle_file_path'])
+	Helper::VersionWorker.save_version_code(ENV['alias'],
+			ENV['build_gradle_file_path'])
+	Helper::VersionWorker.save_beta_version_name(ENV['alias'],
+			     ENV['build_gradle_file_path'])
       end
 
       def self.description
