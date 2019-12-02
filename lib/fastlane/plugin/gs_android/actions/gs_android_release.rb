73ddcea197f57a77a59da4554e554638755821a5
nailgilaziev <nail.gilaziev@gmail.com>
2019-12-02 23:26:51 +0300

    debug mg

--- lib/fastlane/plugin/gs_android/actions/gs_android_release.rb
+++ lib/fastlane/plugin/gs_android/actions/gs_android_release.rb
@@ -2,9 +2,7 @@ module Fastlane
   module Actions
     class GsAndroidReleaseAction < Action
       def self.run(params)
-        UI.message(' Release action started for ' + ENV['alias'])
-        version_name = Helper::VersionWorker.get_rc_version_name(ENV['alias']).ignore_build.to_s
-        
+        UI.message('ReleaseAction with params = ' + params + ' started for ' + ENV['alias'])
         Helper::GsAndroidHelper.run_action(Actions::SupplyAction,
                                            track: 'beta',
                                            track_promote_to: 'production',
@@ -12,14 +10,15 @@ module Fastlane
                                            skip_upload_metadata: true,
                                            skip_upload_images: true,
                                            skip_upload_screenshots: true)
-        
+        version_name = Helper::VersionWorker.get_rc_version_name(ENV['alias']).ignore_build.to_s
+        UI.message('SupplyAction complete. Start update_changelog for version_name = ' + version_name)        
         Helper::GooglePlayLoader.update_changelog(ENV['alias'],
                                                   ENV['app_id'],
                                                   version_name,
                                                   'production',
                                                   ENV['locales'],
                                                   ENV['json_key_file'])
-
+        UI.message('call save_release_version_name')        
         Helper::VersionWorker.save_release_version_name(ENV['alias'])
       end
 
